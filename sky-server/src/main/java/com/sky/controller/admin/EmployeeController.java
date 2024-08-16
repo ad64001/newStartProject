0e228000e72f179efa228567fd9db344b57b7ba5
“ad64001” <3192364930@qq.com>
2024-08-16 17:14:28 +0800

    添加Eee 功能

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -73,6 +74,15 @@ public class EmployeeController {
     @PostMapping("/logout")
     @ApiOperation(value = "员工退出")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+    @PostMapping
+    @ApiOperation(value = "新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        log.info("new Add Employee:{}",employeeDTO);
+        System.out.println("現在のスレッドID:"+Thread.currentThread().getId());
+        employeeService.save(employeeDTO);
         return Result.success();
     }
 
